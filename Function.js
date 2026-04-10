699f14b4f460d7d059f9ec4ae7ffdde613aee9f7
johnjoalex32-source <johnjoalex32@gmail.com>
2026-04-10 17:12:41 +0200

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,6 @@
+function myFunction() {
+document.getElementById("result").innerHTML =
+document.getElementById("demo").firstChild.nodeValue; 
+document.getElementById("result1").innerHTML =
+document.getElementById("demo1").childNodes[0].nodeValue; 
+}
