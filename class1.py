16951558f01c053a34604cd0f2109c00aec6e58b
johnjoalex32-source <johnjoalex32@gmail.com>
2026-07-04 18:05:54 +0200

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,49 @@
+"""print("Hello")
+print("Welcome to the World")"""
+#print(5)
+
+# Printing a greeting message
+print("Hello, world!")
+
+# Printing my name
+print("My name is Alex.")
+
+# Printing a fun fact about myself
+# print("I love playing soccer.")
+print("I love playing soccer.")
+
+# Performing and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)
+
+# Performing and printing a simple calculation
+print("The result of 91 - 11 is:", 91 - 11)
+
+# Performing and printing a simple calculation
+print("The result of 2 * 111 is:", 2 * 111)
+
+# Performing and printing a simple calculation
+print("The result of 12 / 6 is:", 12 / 6)
+
+# Performing and printing a simple calculation
+print("The result of 399112 + 500 is:", 399112 + 500)
+
+# Performing and printing a simple calculation
+print("The result of 3 * 5 is:", 3 * 5)
+
+# Performing and printing a simple calculation
+print("The result of 31 / 51 is:", 31 / 51)
+
+# Performing and printing a simple calculation
+print("The result of 3 - 511 is:", 3 - 511)
+
+# Performing and printing a simple calculation
+print("The result of 3M + 5M is:", 3000000 + 5000000)
+
+# Performing and printing a simple calculation
+print("The result of 3 - 5 is:", 3 - 5)
+
+# Performing and printing a simple calculation
+print("The result of 3 / 5 is:", 3 / 5)
+
+# Performing and printing a simple calculation
+print("The result of 30 ** 50 is:", 30 ** 50)
