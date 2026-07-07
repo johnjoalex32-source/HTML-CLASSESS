0c507b980ab3cb9061ad505a8abf0469955e3689
johnjoalex32-source <johnjoalex32@gmail.com>
2026-07-07 12:11:33 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# Take user inputs
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+grade = input("Enter your grade: ")
+hobby = input("Enter your hobby: ")
+favouriteanimal = input("Enter your favouriteanimal: ")
+school = input("Enter your school: ")
+
+# Print a formatted name tag
+print("\n--- NAME TAG ---")
+print("----------------")
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("Grade:", grade)
+print("Hobby:", hobby)
+print("favouriteanimal:", favouriteanimal)
+print("school:", school)
+
+# Loading dots using end=
+print("\nLoading", end="")
+print(".", end="")
+print(".", end="")
+print(".")
+print(".", end="")
+print(".", end="")
+print(".", end="")
+print(".", end="")
