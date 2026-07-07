8d0a5027be3d76e010b09a911bd31144c70ead21
johnjoalex32-source <johnjoalex32@gmail.com>
2026-07-07 12:55:13 +0200

    Add files via upload

---
+++ Storing-Birthdays.py
@@ -0,0 +1,23 @@
+# Take user inputs
+adriandate = input("Enter your adriandate: ")
+bobdate = input("Enter your bobdate: ")
+codydate = input("Enter your codydate: ")
+danieldate = input("Enter your danieldate: ")
+elondate = input("Enter your elondate: ")
+
+# Print a formatted name tag
+print("\n--- NAME TAG ---")
+print("----------------")
+print("AdrianDate:", adriandate)
+print("BobDate:", bobdate)
+print("CodyDate:", codydate)
+print("DanielDate:", danieldate)
+print("ElonDate:", elondate)
+
+# Loading dots using end=
+print("\nLoading", end="")
+print(".", end="")
+print(".", end="")
+print(".", end="")
+print(".", end="")
+print(".", end="")
