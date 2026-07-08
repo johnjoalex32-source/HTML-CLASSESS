11fd316dbfc1252bf86d47d2adc58a111ccd74c1
johnjoalex32-source <johnjoalex32@gmail.com>
2026-07-08 14:31:12 +0200

    Add files via upload

---
+++ Congratulations-Message.py
@@ -0,0 +1,40 @@
+str1 = 'Hello'
+str2 = 'Ram!'
+
+# using +
+print('str1 + str2 =', str1 + str2)
+
+
+message = "congratulations"
+print(message.upper())
+
+a= "congratulations"
+print("type of a: ", type(a))
+
+# Assigning Different Variables
+name = "Ram"
+age = 15
+is_student = True
+weight = 38.5
+
+# Printing Different Variables and their Data Type
+print("Name :", name)
+print("Data Type of Name is", type(name))
+
+print("Age :", age)
+print("Data Type of Age is", type(age))
+
+print("is_student :", is_student)
+print("Data Type of is_student is", type(is_student))
+
+print("Weight :", weight)
+print("Data Type of weight is", type(weight))
+
+# Type casting to convert the datatype of variables
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
